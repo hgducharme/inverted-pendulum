3dfa3076f8be286722a702d72334f42a26967b77
Hunter Ducharme <hgducharme@gmail.com>
2019-08-09 23:37:18 -0500

    refactored code and started state space analysis

--- invertedpendulum/invertedpendulum.py
+++ invertedpendulum/invertedpendulum.py
@@ -3,9 +3,16 @@ import numpy as np
 
 _g = 9.807
 
-class InvertedPendulum():
 
-    def __init__(self, massCart = None, massPendulum = None, lengthCM = None, totalLength = None, frictionCoeff = None):
+class InvertedPendulum:
+    def __init__(
+        self,
+        massCart=None,
+        massPendulum=None,
+        lengthCM=None,
+        totalLength=None,
+        frictionCoeff=None,
+    ):
         self.massCart = massCart
         self.massPendulum = massPendulum
         self.lengthCM = lengthCM
@@ -16,108 +23,106 @@ class InvertedPendulum():
         return "System"
 
     @property
-    def massCart(self):
-        return self._massCart
-    
-    @property
-    def massPendulum(self):
-        return self._massPendulum
-
-    @property
     def massSystem(self):
-        return self._massCart + self._massPendulum
-
-    @property
-    def lengthCM(self):
-        return self._lengthCM
-
-    @property
-    def totalLength(self):
-        return self._totalLength
-
-    @property
-    def frictionCoeff(self):
-        return self._frictionCoeff
+        return self.massCart + self.massPendulum
 
     @property
     def MOI(self):
-        return (1.0/12.0) * self._massPendulum * (self._totalLength**2.0)
+        return (1.0 / 12.0) * self.massPendulum * (self.totalLength ** 2.0)
 
     @property
     def _beta(self):
-        return self._massPendulum * self._lengthCM/(self._massPendulum * (self._lengthCM**2.0) + self.MOI)
+        return (
+            self.massPendulum
+            * self.lengthCM
+            / (self.massPendulum * (self.lengthCM ** 2.0) + self.MOI)
+        )
 
     @property
-    def _denominator(self):
-        return self.massSystem - (self._beta * self._massPendulum * self._lengthCM)
+    def controlLaw(self):
+        return self._controlLaw
 
-    @massCart.setter
-    def massCart(self, value):
-        self._massCart = value
-
-    @massPendulum.setter
-    def massPendulum(self, value):
-        self._massPendulum = value
-
-    @lengthCM.setter
-    def lengthCM(self, value):
-        self._lengthCM = value
-
-    @totalLength.setter
-    def totalLength(self, value):
-        self._totalLength = value
-
-    @frictionCoeff.setter
-    def frictionCoeff(self, value):
-        self._frictionCoeff = value
+    @controlLaw.setter
+    def controlLaw(self, value):
+        self._controlLaw = value
 
     @property
-    def nonlinearModel(self):
-        return self._nonlinearModel
+    def nonLinearModel(self):
+        return self._nonLinearModel
 
-    @nonlinearModel.setter
-    def nonlinearModel(self, value):
+    @nonLinearModel.setter
+    def nonLinearModel(self, value):
 
         stateVector, controlInput = value
 
         # Define short hand for variables
-        x1 = stateVector[0] # theta (rad)
-        x2 = stateVector[1] # x
-        x3 = stateVector[2] # theta dot (rad)
-        x4 = stateVector[3] # x dot
+        x1 = stateVector[0]  # theta (rad)
+        x2 = stateVector[1]  # x
+        x3 = stateVector[2]  # theta dot (rad)
+        x4 = stateVector[3]  # x dot
         u = controlInput
 
         # System of nonlinear first order differential equations
-        nonlinearDenominator = self.massSystem - ( self._beta * self._massPendulum * self._lengthCM * (np.cos(x1)**2) )
+        nonlinearDenominator = self.massSystem - (
+            self._beta * self.massPendulum *
+            self.lengthCM * (np.cos(x1) ** 2)
+        )
 
         x1Dot = x3
         x2Dot = x4
-        x3Dot = (self._beta/nonlinearDenominator) * ( (_g * self.massSystem * np.sin(x1)) + (self._frictionCoeff * x4 - u) * np.cos(x1) - (self._massPendulum * self._lengthCM * (x3**2) * np.sin(x1) * np.cos(x1)) )
-        x4Dot = (1/nonlinearDenominator) * ( u - (self._frictionCoeff * x4) + self._massPendulum * self._lengthCM * np.sin(x1)*( (x3**2) - (_g * self._beta * np.cos(x1)) ))
+        x3Dot = (self._beta / nonlinearDenominator) * (
+            (_g * self.massSystem * np.sin(x1))
+            + (self.frictionCoeff * x4 - u) * np.cos(x1)
+            - (
+                self.massPendulum
+                * self.lengthCM
+                * (x3 ** 2)
+                * np.sin(x1)
+                * np.cos(x1)
+            )
+        )
+        x4Dot = (1 / nonlinearDenominator) * (
+            u
+            - (self.frictionCoeff * x4)
+            + self.massPendulum
+            * self.lengthCM
+            * np.sin(x1)
+            * ((x3 ** 2) - (_g * self._beta * np.cos(x1)))
+        )
 
-        vectorField = [x1Dot, x2Dot, x3Dot, x4Dot]
-
-        self._nonlinearModel = vectorField
+        self._nonLinearModel = [x1Dot, x2Dot, x3Dot, x4Dot]
 
     @property
     def stateSpace(self):
         return self._stateSpace
-    
+
     @stateSpace.setter
-    def stateSpace(self, equilibriumPoint):
-        A31 = _g * self._beta * self.massSystem * equilibriumPoint/self._denominator
-        A34 = self._frictionCoeff * self._beta * equilibriumPoint/self._denominator
-        A41 = -self._massPendulum * _g * self._lengthCM * self._beta/self._denominator
-        A44 = -self._frictionCoeff/self._denominator
+    def stateSpace(self, equilibriumPoint=None):
+        denominator = self.massSystem - (self._beta * self.massPendulum * self.lengthCM)
 
-        B31 = -self._beta * equilibriumPoint/self._denominator
-        B41 = 1/self._denominator
+        A31 = _g * self._beta * self.massSystem * equilibriumPoint / denominator
+        A34 = self.frictionCoeff * self._beta * equilibriumPoint / denominator
+        A41 = -self.massPendulum * _g * self.lengthCM * self._beta / denominator
+        A44 = -self.frictionCoeff / denominator
 
-        A = [ [0, 0, 1, 0], [0, 0, 0, 1], [A31, 0, 0, A34], [A41, 0, 0, A44] ]
-        B = [ [0], [0], [B31], [B41] ]  
-        C = [ [1, 0, 0, 0], [0, 1, 0, 0] ]
-        D = [ [0], [0] ]
+        B31 = -self._beta * equilibriumPoint / denominator
+        B41 = 1 / denominator
 
-        model = ss(A, B, C, D)
+        A = [[0, 0, 1, 0], [0, 0, 0, 1], [A31, 0, 0, A34], [A41, 0, 0, A44]]
+        B = [[0], [0], [B31], [B41]]
+        C = [[1, 0, 0, 0], [0, 1, 0, 0]]
+        D = [[0], [0]]
 
-        self._stateSpace = model
+        self._stateSpace = ss(A, B, C, D)
+
+    def modelIntegrator(self, time, state):
+        """
+
+        scipy.integrate.solve_ivp() requires that a function only take inputs t and y.
+        A work around for supplying additional arguments is to use a wrapper function like this.
+        see: https://stackoverflow.com/questions/48245765/pass-args-for-solve-ivp-new-scipy-ode-api
+
+        """
+
+        self.nonLinearModel = (state, self.controlLaw)
+        return self.nonLinearModel
--- invertedpendulum/simulation.py
+++ invertedpendulum/simulation.py
@@ -1,6 +1,7 @@
 import numpy as np
 from control.matlab import *
 from scipy.integrate import solve_ivp
+from scipy import signal
 
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
@@ -10,87 +11,126 @@ from matplotlib import animation
 from invertedpendulum import InvertedPendulum
 
 
-def modelWrapper(t, y):
-    """
+def PID(kp, ki, kd, filter_coefficient, reference, value, previous_value, dt):
+    error = value - reference
+    proportional = kp * error
+    integral = ki * error * dt
+    derivative = kd * (error - previous_error)
 
-    scipy.integrate.solve_ivp() requires that a function only take inputs t and y.
-    A work around for supplying additional arguments is to use a wrapper function like this.
-    see: https://stackoverflow.com/questions/48245765/pass-args-for-solve-ivp-new-scipy-ode-api
+def plot_results(time, theta, cart_position):
+    fig, ax = plt.subplots(2, 1, sharex = True, sharey = False)
+    ax[0].plot(time, np.rad2deg(theta))
+    ax[1].plot(time, cart_position * (100.0 / 2.54))
+    ax[0].set(ylabel="theta [deg]")
+    ax[1].set(xlabel="time [s]", ylabel="cart position [in]")
+    ax[1].tick_params()
+    plt.show()
 
-    """
 
-    sys = InvertedPendulum(
-        massCart=0.25,
-        massPendulum=0.125,
-        lengthCM=0.15,
-        totalLength=0.30,
-        frictionCoeff=2.5,
+def make_animation(solution):
+
+    def init_animation():
+        ax.add_patch(cart)
+        ax.add_line(pendulum)
+
+        return (cart, pendulum)
+
+    def animate(iter):
+        # Define and caclulate cart parameters
+        cart_width = 2.50
+        cart_height = 2.125
+        cart_bottom_left_x_position = solution.y[1][iter] - (cart_width / 2.0)
+        cart_center = [cart_bottom_left_x_position + (0.5 * cart_width), 0.5 * cart_height]
+
+        # Define and calculate pendulum paramters
+        pendulum_length = 12
+        angle = solution.y[0][iter]
+        pendulum_end_point = [
+            cart_center[0] - pendulum_length * np.sin(angle),
+            pendulum_length * np.cos(angle),
+        ]
+
+        # Update positions
+        cart.set_width(cart_width)
+        cart.set_height(cart_height)
+        cart.set_xy([cart_bottom_left_x_position, 0])
+
+        pendulum.set_xdata([cart_center[0], pendulum_end_point[0]])
+        pendulum.set_ydata([cart_center[1], pendulum_end_point[1]])
+
+        return (cart, pendulum)
+
+    # Initialize the simulation figure
+    linear_rails_length = 17.60
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    plt.axhline(0, xmin=-9, xmax=9, linewidth="0.75", color="k")
+    left_constraint = plt.vlines(
+        -linear_rails_length / 2.0, ymin=0, ymax=1, linestyle="-", color="r"
     )
-    control_law = 0
-    sys.nonlinearModel = (y, control_law)
+    right_constraint = plt.vlines(
+        linear_rails_length / 2.0, ymin=0, ymax=1, linestyle="-", color="r"
+    )
 
-    return sys.nonlinearModel
+    ax.set(xlim=(-15, 15), ylim=(-15, 15))
+    plt.gca().set_aspect("equal")
+    ax.set(xlabel="x-position [in]", ylabel="y-position [in]")
 
+    # Create the cart and pendulum shapes
+    cart = patches.Rectangle((0, 0), 0, 0, fc="k", fill=False)
+    pendulum = lines.Line2D([], [], linewidth=4, color="c")
 
-# Solve and plot the non linear dynamics
-solution = solve_ivp(modelWrapper, [0, 30], [0, 0, 0.1, 0])
+    # Define the animation parameters
+    frames_per_second = 60
+    interval = 1 / frames_per_second
 
-fig, ax = plt.subplots(1, 1)
-ax.plot(solution.t, np.rad2deg(solution.y[0]))
-ax.set(xlabel="time [s]", ylabel="theta [deg]")
+    anim = animation.FuncAnimation(
+        fig,
+        animate,
+        init_func=init_animation,
+        frames=len(solution.t),
+        interval=1000 * interval,
+        blit=True,
+    )
 
-fig, ax = plt.subplots(1, 1)
-ax.plot(solution.t, solution.y[1] * (100.0 / 2.54))
-ax.set(xlabel="time [s]", ylabel="Cart position [in]")
-
-# Initialize the simulation figure
-linear_rails_length = 17.60
-fig, ax = plt.subplots(figsize=(10, 7))
-plt.axhline(0, xmin=-9, xmax=9, linewidth="0.75", color='k')
-left_constraint = plt.vlines(-linear_rails_length/2.0, ymin=-1, ymax=1, linestyle="dashdot", color='r')
-right_constraint = plt.vlines(linear_rails_length/2.0, ymin=-1, ymax=1, linestyle="dashdot", color='r')
-
-ax.set(xlim=(-20, 20), ylim=(-20, 20))
-
-# Create the cart and pendulum shapes
-cart = patches.Rectangle((0, 0), 0, 0, fc="k", fill=False)
-pendulum = lines.Line2D([], [], linewidth = 3, color = 'c')
-pendulum = lines.Line2D([], [], linewidth = 3, color = 'c')
-pendulum = lines.Line2D([], [], linewidth = 3, color = 'c')
+    plt.show()
 
 
-def init():
-    ax.add_patch(cart)
-    ax.add_line(pendulum)
-
-    return (cart, pendulum, )
+if __name__ == "__main__":
 
 
-def animate(iter):
-    # Define and caclulate cart parameters
-    cart_width = 2.50
-    cart_height = 2.125
-    cart_bottom_left_x_position = solution.y[1][iter] - (cart_width / 2.0)
-    cart_center = [cart_bottom_left_x_position + (0.5*cart_width), 0.5*cart_height]
+    # Create an inverted pendulum system and define the control law
+    sys = InvertedPendulum(massCart=0.3, massPendulum=0.125, lengthCM=0.15, totalLength=0.30, frictionCoeff=2.5)
+    sys.controlLaw = 0
+    initialConditions = [0, 0, 0.1, 0]
+    timeSpan = [0, 9]
+    stepSize = 0.01
 
-    # Define and calculate pendulum paramters
-    pendulum_length = 12
-    angle = solution.y[0][iter]
-    pendulum_end_point = [ cart_center[0] - pendulum_length * np.sin(angle), pendulum_length * np.cos(angle)]
+    # Solve and plot the non linear dynamics
+    solution = solve_ivp(sys.modelIntegrator, [0, 9], [0, 0, 0.1, 0], max_step = stepSize)
+    plot_results(solution.t, solution.y[0], solution.y[1])
+    make_animation(solution)
 
-    # Update positions
-    cart.set_width(cart_width)
-    cart.set_height(cart_height)
-    cart.set_xy([cart_bottom_left_x_position, 0])
+    # Create the state space model to start controlling the dynamics
+    sys.stateSpace = (1)
+    model = sys.stateSpace
+    controllabilityMatrix = ctrb(model.A, model.B)
+    observabilityMatrix = obsv(model.A, model.C)
+    eigenvalues, eigenvectors = np.linalg.eig(model.A)
 
-    pendulum.set_xdata( [cart_center[0], pendulum_end_point[0]] )
-    pendulum.set_ydata( [cart_center[1], pendulum_end_point[1]] )
+    if np.linalg.matrix_rank(controllabilityMatrix) == 4:
+        print('The system is controllable.')
+    else:
+        print('The system is not controllable.')
 
-    return (cart, pendulum, )
-
-
-anim = animation.FuncAnimation(
-    fig, animate, init_func=init, frames=len(solution.t), interval=75, blit=True
-)
-
-plt.show()
+    if np.linalg.matrix_rank(observabilityMatrix) == 4:
+        print('The system is observable.')
+    else:
+        print('The system is not observable.')
+    
+    # This needs work.
+    timeSpan = np.linspace(0, 20, 100)
+    t, y = step(model, timeSpan)
+    plt.plot(y, t)
+    plt.legend(['y1','y2'],loc='best')
+    plt.show()
