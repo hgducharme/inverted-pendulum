713ec03fe983617e6db39599b8f4437b72d90fb7
Hunter Ducharme <hgducharme@gmail.com>
2019-08-19 23:21:37 -0500

    getting closer
    I was wrong. We dont want to get rid of interrupts because thats how we keep track of our encoder changes. What I was trying to say is that we dont need to check if the current value has changed from the previous value. That was just in place to not clutter the serial, but now that we print every specified timeframe that will do the job.

--- arduino/invertedpendulum/src/main.cpp
+++ arduino/invertedpendulum/src/main.cpp
@@ -1,5 +1,4 @@
 #define ENCODER_OPTIMIZE_INTERRUPTS
-
 #include <Arduino.h>
 #include <Encoder.h>
 #include "motorControllerDrokL298.h"
@@ -13,51 +12,32 @@
 Encoder cartEncoder(cartEncoderPhaseA, cartEncoderPhaseB);
 Encoder pendulumEncoder(pendulumEncoderPhaseA, pendulumEncoderPhaseB);
 
-long previousCartEncoderCount = -999.0;
-long previousPendulumEncoderCount = -999.0;
+unsigned long timeframe = 5000;
+unsigned long previousMilliseconds = 0;
 
 void sendEncoderValuesToPython(long cartEncoderCount, long pendulumEncoderCount) {
 
-  /*
-  NOTE: We can probably get rid of the attach interrupts and instead just read the encoder values every 0.01 or 0.1 seconds.
-  We would do this because attach interrupts are expensive (I think), but mostly because we would be sending the values
-  to python whenever we DETECT a change, so python doesn't know when it will get a change back, and when it does get a change
-  it doesn't know how many bytes, because we can have several quick changes. I think it would be better just to operate on a discrete
-  known time scale (eg. every 0.1 seconds). 
-  */
+  float cartAngle;           // radians
+  float pendulumAngle;       // radians
+  float cartDistance;        // depends on units of idlerRadius
+  float idlerRadius = 0.189; // inches
 
-    // If one of the encoders changes, update and print values to serial
-  if (cartEncoderCount != previousCartEncoderCount || pendulumEncoderCount != previousPendulumEncoderCount)
-  {
+  // Calculate linear distance travelled by cart x = r * theta
+  cartAngle = (cartEncoderCount / 2400.0) * (360.0);
+  cartDistance = idlerRadius * cartAngle;
 
-    float cartAngle;           // radians
-    float pendulumAngle;       // radians
-    float cartDistance;        // depends on units of idlerRadius
-    float idlerRadius = 0.189; // inches
+  // Calculate pendulum angle
+  pendulumAngle = (pendulumEncoderCount / 2400.0) * (360.0);
 
-    previousCartEncoderCount = cartEncoderCount;
-    previousPendulumEncoderCount = pendulumEncoderCount;
-
-    // Calculate linear distance travelled by cart x = r * theta
-    cartAngle = (cartEncoderCount / 2400.0) * (360.0);
-    cartDistance = idlerRadius * cartAngle;
-
-    // Calculate pendulum angle
-    pendulumAngle = (pendulumEncoderCount / 2400.0) * (360.0);
-
-    Serial.print(cartAngle);
-    Serial.print(".");
-    Serial.print(pendulumAngle);
-    Serial.println();
-  }
+  Serial.print(cartAngle);
+  Serial.print(".");
+  Serial.print(pendulumAngle);
+  Serial.println();
 }
 
-unsigned long timeframe = 50;
-unsigned long previousMilliseconds = 0;
 void setup()
 {
-
-  Serial.begin(115200);
+  Serial.begin(9400);
 
   // Motor controller
   pinMode(IN1, OUTPUT);
@@ -73,15 +53,16 @@ void loop()
 
   if ( (currentMilliseconds - previousMilliseconds) > timeframe ) {
     sendEncoderValuesToPython(cartEncoderCount, pendulumEncoderCount);
+    previousMilliseconds = millis();
   }
 
   // moveCart('R', 20);
-  // delay(3000);
+  // delay(100);
 
   // brake();
 
   // moveCart('L', 20);
-  // delay(3000);
+  // delay(100);
 
   // brake();
 }
--- invertedpendulum/live_arduino_control.py
+++ invertedpendulum/live_arduino_control.py
@@ -1,11 +1,19 @@
 import serial
 
+"""
+This script needs work. We need to find a way to read lines every 0.1 seconds.
+We also need to find a way to differentiate empty lines from lines with data we want.
+see: https://stackoverflow.com/questions/1093598/pyserial-how-to-read-the-last-line-sent-from-a-serial-device
+
+I'm calling it a night
+"""
+
 # Define the serial port and baud rate.
 _BAUD_RATE = 115200
 
-arduino = serial.Serial('/dev/cu.usbmodem14101', _BAUD_RATE, timeout = 0.1)
+arduino = serial.Serial('/dev/cu.usbmodem14101', 9400, timeout = 0)
 
 if __name__ == "__main__":
     while True:
         line = arduino.readlines()
-        print(line)
+        print(line[0]) if line else None
