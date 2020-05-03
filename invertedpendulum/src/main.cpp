b5a1caaa4bf80f5e9ac6045857ddd8f69241988d
Hunter Ducharme <hgducharme@gmail.com>
2020-05-03 01:21:40 -0500

    Added previousState and moved things around

--- invertedpendulum/src/main.cpp
+++ invertedpendulum/src/main.cpp
@@ -9,12 +9,11 @@
 #include "Cart.hpp"
 #include "EncoderWrapper.hpp"
 
-// Initialize encoders
+// Define pins
 #define cartEncoderPhaseA 3
 #define cartEncoderPhaseB 4
 #define pendulumEncoderPhaseA 2
 #define pendulumEncoderPhaseB 5
-
 const int motorChannelIN1 = 7;
 const int motorChannelIN2 = 8;
 const int motorChannelENA = 9;
@@ -22,22 +21,26 @@ const int motorChannelENA = 9;
 // Initialize variables and named constants
 const double ENCODER_PPR = 2400.0;
 const double IDLER_PULLEY_RADIUS = 0.0048006;   // meters
-const unsigned long TIMEFRAME = 3;              // milliseconds
+const unsigned long LOOP_RATE = 3;              // milliseconds
 const double ANGLE_BOUND = 30.0 * (PI / 180.0); // radians
-double previousCartPosition = 0.0;              // meters
-double previousPendulumAngle = PI;              // radians
 unsigned long previousMilliseconds = 0;
 double gainVector[4] = {-2000.0, 900.0, -100.0, 300.0};
 
-LQRController LQR(gainVector);
-StateVector state(0, 0, 5, 6);
-DrokL928 motorController(motorChannelIN1, motorChannelIN2, motorChannelENA);
-Cart cart(&motorController);
-
+// Initialize hardware layer objects
 Encoder ce(cartEncoderPhaseA, cartEncoderPhaseB);
 Encoder pe(pendulumEncoderPhaseA, pendulumEncoderPhaseB);
 EncoderWrapper cartEncoder(ce, ENCODER_PPR);
 EncoderWrapper pendulumEncoder(pe, ENCODER_PPR);
+DrokL928 motorController(motorChannelIN1, motorChannelIN2, motorChannelENA);
+
+// Initialize application layer objects
+Cart cart(&motorController);
+LQRController LQR(gainVector);
+StateVector state(0, 0, 5, 6);
+StateVector previousState(0, 0, 0, PI);
+// TODO:
+// Scheduler?
+// StateCalculator?
 
 void setup()
 {
@@ -55,14 +58,14 @@ void loop()
   long pendulumEncoderCount = pendulumEncoder.read();
 
   // Send values to python every n milliseconds
-  if ((currentMilliseconds - previousMilliseconds) >= TIMEFRAME)
+  if ((currentMilliseconds - previousMilliseconds) >= LOOP_RATE)
   {
 
     // Compute the state
     state.pendulumAngle = encoderCountToPendulumAngleRadians(pendulumEncoderCount, ENCODER_PPR);          // radians
     state.cartPosition = encoderCountToCartPosition(cartEncoderCount, ENCODER_PPR, IDLER_PULLEY_RADIUS);  // meters
-    state.pendulumAngularVelocity = (state.pendulumAngle - previousPendulumAngle) / (TIMEFRAME / 1000.0); // radians/s
-    state.cartVelocity = (state.cartPosition - previousCartPosition) / (TIMEFRAME / 1000.0);              // meters/s
+    state.pendulumAngularVelocity = (state.pendulumAngle - previousState.pendulumAngle) / (LOOP_RATE / 1000.0); // radians/s
+    state.cartVelocity = (state.cartPosition - previousState.cartPosition) / (LOOP_RATE / 1000.0);              // meters/s
 
     // Compute control input using LQR and handle saturation
     // NOTE: A PWM value of 35 is essentially the lowest value to start moving the cart due to friction
@@ -71,7 +74,9 @@ void loop()
 
     // Store the current data for computation in the next loop
     previousMilliseconds = currentMilliseconds;
-    previousPendulumAngle = state.pendulumAngle;
-    previousCartPosition = state.cartPosition;
+
+    // TODO: previousState.store(currentState) ?
+    previousState.pendulumAngle = state.pendulumAngle;
+    previousState.cartPosition = state.cartPosition;
   }
 }
