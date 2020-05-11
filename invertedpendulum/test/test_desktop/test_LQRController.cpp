1450a3e2ac08aaca913fd0c9450df3d5cdbef1f9
Hunter Ducharme <hgducharme@gmail.com>
2020-05-10 21:46:59 -0500

    Removed setUp function

--- invertedpendulum/test/test_desktop/test_LQRController.cpp
+++ invertedpendulum/test/test_desktop/test_LQRController.cpp
@@ -7,28 +7,26 @@
 static const double ANGLE_BOUND = 30.0 * (M_PI / 180.0); // radians
 double gainVector[4] = {-2000.0, 900.0, -100.0, 300.0};
 
-void setUp(void) {
-    LQRController controller(gainVector, ANGLE_BOUND);
-}
-
 void test_controlInputIsCalculatedCorrectly(void)
 {
     double pendulumAngle = 10.0 * (M_PI / 180.0);
     StateVector state(0.0872665, 0.1, 5.0, 6.0);
+    LQRController controller(gainVector, ANGLE_BOUND);
 
     double controlInput = controller.computeControlInput(state);
-
     double expectedResult = gainVector[0]*state.pendulumAngle + gainVector[1]*state.cartPosition + gainVector[2]*state.pendulumAngularVelocity + gainVector[3]*state.cartVelocity;
+
     TEST_ASSERT_EQUAL(expectedResult, controlInput);
 }
 
 void test_controlInputIsZeroWhenPendulumIsOutOfBounds(void) {
     double pendulumAngle = 31.0 * (M_PI / 180.0);
     StateVector state(pendulumAngle, 0.1, 5.0, 6.0);
+    LQRController controller(gainVector, ANGLE_BOUND);
 
     double controlInput = controller.computeControlInput(state);
-
     double expectedResult = 0;
+    
     TEST_ASSERT_EQUAL(expectedResult, controlInput);
 }
 
